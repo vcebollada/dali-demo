ca40df93b64d277101687143ec312a38ab4e642c
Anton Obzhirov <a.obzhirov@samsung.com>
2019-09-24 11:08:21 +0100

    Fix cmake script for dali-core to build two targets.
    Change-Id: I2a14efd22d5c55c617a40879a19d471ed569f098

--- android/native-demo/dali/build_core.sh
+++ android/native-demo/dali/build_core.sh
@@ -1,7 +1,12 @@
-cd ../../../../dali-core/build/tizen
-~/Android/Sdk/cmake/3.10.2.4988404/bin/cmake ./ -DCMAKE_C_COMPILER=~/Android/Sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin/clang -DCMAKE_CXX_COMPILER=~/Android/Sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ -DCMAKE_INSTALL_PREFIX=~/dali-android/dali/dali-env/android -DCMAKE_TOOLCHAIN_FILE=~/Android/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=24 -DANDROID_ABI=${TARGET} -DANDROID_NDK=${HOME}/Android/Sdk/ndk-bundle -DANDROID_STL=c++_shared -DCMAKE_CXX_FLAGS='-fexceptions -frtti -w -Wall -std=gnu++11' -DCMAKE_BUILD_TYPE=Release
+
+if [ -d ./dali-core/cmake/${TARGET} ]
+then
+cd ./dali-core/cmake/${TARGET}
+else
+mkdir -p ./dali-core/cmake/${TARGET}
+cd ./dali-core/cmake/${TARGET}
+~/Android/Sdk/cmake/3.10.2.4988404/bin/cmake ../../../../../../../dali-core/build/tizen -DCMAKE_C_COMPILER=~/Android/Sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin/clang -DCMAKE_CXX_COMPILER=~/Android/Sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ -DCMAKE_INSTALL_PREFIX=~/dali-android/dali/dali-env/android -DCMAKE_TOOLCHAIN_FILE=~/Android/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=24 -DANDROID_ABI=${TARGET} -DANDROID_NDK=${HOME}/Android/Sdk/ndk-bundle -DANDROID_STL=c++_shared -DCMAKE_CXX_FLAGS='-fexceptions -frtti -w -Wall -std=gnu++11' -DLIB_DIR=~/dali-android/dali/dali-env/android/lib/${TARGET} -DCMAKE_BUILD_TYPE=Release
+fi
 make
 make install
-mkdir -p ~/dali-android/dali/dali-env/android/lib/${TARGET}
-mv ~/dali-android/dali/dali-env/android/lib/libdali-core.so ~/dali-android/dali/dali-env/android/lib/${TARGET}/libdali-core.so
 cd -
