b9fe80fc488b15159c1833e2500be420ea32ce78
Anton Obzhirov <a.obzhirov@samsung.com>
2019-10-16 22:11:06 +0100

    Update build_freetype.sh to resolve deps.
    Change-Id: I2661f079cfe9cf0eaaa8ff90c7f907020efb9368

--- android/native-demo/dali/build_freetype.sh
+++ android/native-demo/dali/build_freetype.sh
@@ -5,9 +5,14 @@ else
 mkdir -p ./libfreetype/cmake/${TARGET}
 cd ./libfreetype/cmake/${TARGET}
 export PREFIX=${DALI_DIR}/dali-env/android
+export LIB_DIR=${PREFIX}/lib/${TARGET}
+export INCLUDE_DIR=${PREFIX}/include
 export PKG_CONFIG_LIBDIR=${ANDROID_NDK}/prebuilt/linux-x86_64/lib/pkgconfig
-export PKG_CONFIG_PATH=${PREFIX}/lib/${TARGET}/pkgconfig
-${ANDROID_SDK}/cmake/3.10.2.4988404/bin/cmake ${DALI_DIR}/android-dependencies/libfreetype -DCMAKE_C_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang -DCMAKE_CXX_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=24 -DANDROID_ABI=${TARGET} -DANDROID_NDK=${ANDROID_NDK} -DANDROID_STL=c++_shared -DCMAKE_CXX_FLAGS='-fexceptions -frtti -w -Wall -std=gnu++11' -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib/${TARGET} -DCMAKE_BUILD_TYPE=$@ -DPNG_DIR=${PREFIX}/lib/${TARGET}/pkgconfig -DBUILD_SHARED_LIBS=ON -DFT_WITH_ZLIB=ON -DFT_WITH_BZIP2=ON -DFT_WITH_PNG=ON -DFT_WITH_HARFBUZZ=ON
+export PKG_CONFIG_PATH=${LIB_DIR}/pkgconfig
+echo ${CMAKE_MODULE_PATH}
+echo ${LIB_DIR}
+echo ${INCLUDE_DIR}
+${ANDROID_SDK}/cmake/3.10.2.4988404/bin/cmake ${DALI_DIR}/android-dependencies/libfreetype -DCMAKE_C_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang -DCMAKE_CXX_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=24 -DANDROID_ABI=${TARGET} -DANDROID_NDK=${ANDROID_NDK} -DANDROID_STL=c++_shared -DCMAKE_CXX_FLAGS='-I\ ${DALI_DIR}/android-dependencies/libfreetype -I\ ${DALI_DIR}/android-dependencies/libfreetype/include/freetype/config -fexceptions -frtti -w -Wall -std=gnu++11' -DCMAKE_INSTALL_INCLUDEDIR=${INCLUDE_DIR} -DCMAKE_INSTALL_LIBDIR=${LIB_DIR} -DCMAKE_BUILD_TYPE=$@ -DBUILD_SHARED_LIBS=ON -DFT_WITH_ZLIB=ON -DFT_WITH_PNG=ON -DFT_WITH_HARFBUZZ=ON -DPNG_LIBRARY=${LIB_DIR} -DPNG_PNG_INCLUDE_DIR=${INCLUDE_DIR} -DHARFBUZZ_LIBRARIES=${LIB_DIR} -DHARFBUZZ_INCLUDE_DIRS=${INCLUDE_DIR}
 fi
 make
 make install
