b9ecca5afefd164cb2de6d20ddfaeb4a0ed0c5d4
Anton Obzhirov <a.obzhirov@samsung.com>
2019-10-18 10:16:07 +0100

    Fix cmake build scripts for freetype and harfbuzz to compile, still not linking.
    Change-Id: I7efd9a7868fa4be9729111780c98afa970decb3b

--- android/native-demo/dali/build_freetype.sh
+++ android/native-demo/dali/build_freetype.sh
@@ -9,10 +9,9 @@ export LIB_DIR=${PREFIX}/lib/${TARGET}
 export INCLUDE_DIR=${PREFIX}/include
 export PKG_CONFIG_LIBDIR=${ANDROID_NDK}/prebuilt/linux-x86_64/lib/pkgconfig
 export PKG_CONFIG_PATH=${LIB_DIR}/pkgconfig
-echo ${CMAKE_MODULE_PATH}
-echo ${LIB_DIR}
-echo ${INCLUDE_DIR}
-${ANDROID_SDK}/cmake/3.10.2.4988404/bin/cmake ${DALI_DIR}/android-dependencies/libfreetype -DCMAKE_C_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang -DCMAKE_CXX_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=24 -DANDROID_ABI=${TARGET} -DANDROID_NDK=${ANDROID_NDK} -DANDROID_STL=c++_shared -DCMAKE_CXX_FLAGS='-I\ ${DALI_DIR}/android-dependencies/libfreetype -I\ ${DALI_DIR}/android-dependencies/libfreetype/include/freetype/config -fexceptions -frtti -w -Wall -std=gnu++11' -DCMAKE_INSTALL_INCLUDEDIR=${INCLUDE_DIR} -DCMAKE_INSTALL_LIBDIR=${LIB_DIR} -DCMAKE_BUILD_TYPE=$@ -DBUILD_SHARED_LIBS=ON -DFT_WITH_ZLIB=ON -DFT_WITH_PNG=ON -DFT_WITH_HARFBUZZ=ON -DPNG_LIBRARY=${LIB_DIR} -DPNG_PNG_INCLUDE_DIR=${INCLUDE_DIR} -DHARFBUZZ_LIBRARIES=${LIB_DIR} -DHARFBUZZ_INCLUDE_DIRS=${INCLUDE_DIR}
+${ANDROID_SDK}/cmake/3.10.2.4988404/bin/cmake ${DALI_DIR}/android-dependencies/libfreetype -DCMAKE_C_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang -DCMAKE_CXX_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=24 -DANDROID_ABI=${TARGET} -DANDROID_NDK=${ANDROID_NDK} -DANDROID_STL=c++_shared -DCMAKE_CXX_FLAGS='-fexceptions -frtti -w -Wall -std=gnu++11' -DCMAKE_INSTALL_INCLUDEDIR=include -DCMAKE_INSTALL_LIBDIR=lib/${TARGET} -DCMAKE_BUILD_TYPE=$@ -DBUILD_SHARED_LIBS=ON -DFT_WITH_ZLIB=ON -DFT_WITH_PNG=OFF -DFT_WITH_HARFBUZZ=OFF -DCMAKE_C_FLAGS='-I./include -I./include/freetype -I./include/freetype/config'
+# -DPNG_LIBRARY=${LIB_DIR} -DPNG_PNG_INCLUDE_DIR=${INCLUDE_DIR}
+# -DHARFBUZZ_LIBRARIES=${LIB_DIR} -DHARFBUZZ_INCLUDE_DIRS=${INCLUDE_DIR}/harfbuzz
 fi
 make
 make install
--- android/native-demo/dali/build_harfbuzz.sh
+++ android/native-demo/dali/build_harfbuzz.sh
@@ -8,9 +8,11 @@ if [ -z "$ENABLE_TRACE" ]; then
 export ENABLE_TRACE=OFF
 fi
 export PREFIX=${DALI_DIR}/dali-env/android
+export LIB_DIR=${PREFIX}/lib/${TARGET}
+export INCLUDE_DIR=${PREFIX}/include
 export PKG_CONFIG_LIBDIR=${ANDROID_NDK}/prebuilt/linux-x86_64/lib/pkgconfig
-export PKG_CONFIG_PATH=${PREFIX}/lib/${TARGET}/pkgconfig
-${ANDROID_SDK}/cmake/3.10.2.4988404/bin/cmake ${DALI_DIR}/android-dependencies/libharfbuzz -DCMAKE_C_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang -DCMAKE_CXX_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=24 -DANDROID_ABI=${TARGET} -DANDROID_NDK=${ANDROID_NDK} -DANDROID_STL=c++_shared -DCMAKE_CXX_FLAGS='-fexceptions -frtti -w -Wall -std=gnu++11' -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib/${TARGET} -DCMAKE_BUILD_TYPE=$@ -DHB_BUILTIN_UCDN=ON -DBUILD_SHARED_LIBS=ON
+export PKG_CONFIG_PATH=${LIB_DIR}/pkgconfig
+${ANDROID_SDK}/cmake/3.10.2.4988404/bin/cmake ${DALI_DIR}/android-dependencies/libharfbuzz -DCMAKE_C_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang -DCMAKE_CXX_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=24 -DANDROID_ABI=${TARGET} -DANDROID_NDK=${ANDROID_NDK} -DANDROID_STL=c++_shared -DCMAKE_CXX_FLAGS='-fexceptions -frtti -w -Wall -std=gnu++11 -I${INCLUDE_DIR}/freetype2/freetype -I${INCLUDE_DIR}/freetype2/freetype/config' -DCMAKE_INSTALL_INCLUDEDIR=${INCLUDE_DIR} -DCMAKE_INSTALL_LIBDIR=${LIB_DIR} -DCMAKE_PREFIX_PATH=${LIB_DIR}/pkgconfig -DCMAKE_BUILD_TYPE=$@ -DHB_BUILTIN_UCDN=ON -DHB_HAVE_FREETYPE=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_C_FLAGS='-I${INCLUDE_DIR}/freetype2/freetype -I${INCLUDE_DIR}/freetype2/freetype/config'
 fi
 make
 make install
